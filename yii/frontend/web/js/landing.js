64ceb3fcb00457951d3c6fc53e2c921e3f6af12b
Your Name <you@example.com>
2017-03-18 13:03:58 +0500

    фиксы js

--- yii/frontend/web/js/landing.js
+++ yii/frontend/web/js/landing.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
-	setTimeout(function() {
-		$('.header').css('min-height',window.innerHeight + 'px');
-	}, 1);
+	$('.header').css('min-height',window.innerHeight + 'px');
 });
