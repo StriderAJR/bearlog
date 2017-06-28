3d39074cbe28721d71db94a891f472f6703497e9
Unknown <kristal2992@mail.ru>
2017-06-28 15:06:50 +0500

    New book

---
+++ source/Bearlog.Web/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bearlog.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ source/Bearlog.Web/Models/TranslationModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bearlog.Web.Models
+{
+    public class TranslationModel
+    {
+        public Guid Id { get; set; }
+
+    }
+
+    public class BookModel : TranslationModel
+    {
+        public string AuthorName { get; set; }
+        public string AuthorOriginalName { get; set; }
+        public int Year { get; set; }
+        public List<Chapter> Fragments { get; set; }
+
+    }
+
+    public class Chapter
+    {
+        public Guid Id { get; set; }
+        public List<Fragment> Fragments { get; set; }
+    }
+
+    public class Fragment
+    {
+        public Guid Id { get; set; }
+        public List<FragmentTranslation> Translations { get; set; }
+    }
+}
