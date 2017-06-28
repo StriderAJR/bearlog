44e65e00950cb97a403fade30d9c06aa0ffc40df
Unknown <kristal2992@mail.ru>
2017-06-28 15:24:09 +0500

    auto beta

--- source/Bearlog.Web/Controllers/AccountController.cs
+++ source/Bearlog.Web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Bearlog.Web.Models;
 using Bearlog.Web.Services;
 
@@ -16,9 +17,29 @@ namespace Bearlog.Web.Controllers
             return View();
         }
 
-        public ActionResult Login(AccountModel model)
+        public ActionResult Login(LoginModel model, string returnUrl)
         {
-            return RedirectToAction("Index", "Home");
+            if (ModelState.IsValid)
+            {
+                if (new DbService().ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
         }
 
         public ActionResult Register()
