d33552c1677c74a5fdedf8bf2de8a34eabcaf5b7
Unknown <tbirr@mail.ru>
2017-07-05 14:55:40 +0500

    after navernoe

--- source/Bearlog.Web/Controllers/BookController.cs
+++ source/Bearlog.Web/Controllers/BookController.cs
@@ -1,29 +1,111 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Web;
+
 using System.Web.Mvc;
+
 using Bearlog.Web.Models;
+
 using Bearlog.Web.Services;
 
+
+
 namespace Bearlog.Web.Controllers
+
 {
+
+
+
     public class BookController : Controller
+
     {
+
+        DbService _dbService = new DbService();
+
+
+
         [Authorize]
-       // [HttpPost]
+
        
-        public ActionResult Index()
+
+        // bearlog.org/Book/bookId
+
+        public ActionResult Index(Guid id)
+
         {
-            return View();
+            //Передаем
+
+            // var book = _dbService.GetBook(bookId);
+            BookModel book = new BookModel
+            {
+                Name = "Ведьмак"
+            };
+            return View(book); // Возвращаем в вид МОДЕЛЬ!
+
         }
+
+
 
         [HttpPost]
+
         public ActionResult InsertPart (Part model) // insertPart - название страницы?
+
         {
+
             new DbService().AddPart(model);
+
             return View();
+
         }
 
+
+
+        public ActionResult Add()
+
+        {
+            List<Language> languages = new List<Language>
+            {
+                new Language { Id = Guid.NewGuid(), Name = "Русский"},
+                new Language { Id = Guid.NewGuid(), Name = "Английский"}
+            };
+
+            ViewData["Languages"] = languages;
+
+            return View();
+
+        }
+
+
+
+        [HttpPost]
+
+        public ActionResult Add(BookModel model)
+
+        {
+            List<Language> languages = new List<Language>
+            {
+                new Language { Id = Guid.NewGuid(), Name = "Русский"},
+                new Language { Id = Guid.NewGuid(), Name = "Английский"}
+            };
+
+            ViewData["Languages"] = languages;
+
+            Guid bookId;
+            _dbService.AddBook(model, ((BearlogPrincipal)User).Id, out bookId); // Стало
+
+            return RedirectToAction("Index", new { id = bookId});
+
+        }
     }
 }
+
+
+
+
+
+
+
--- source/Bearlog.Web/Controllers/HomeController.cs
+++ source/Bearlog.Web/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace Bearlog.Web.Controllers
 {
     public class HomeController : Controller
     {
+        DbService _dbService = new DbService();
+
         // GET: Home
         public ActionResult Index()
         {
@@ -18,12 +20,29 @@ namespace Bearlog.Web.Controllers
                 var user = ((BearlogPrincipal) User);
                 var userName = user.UserName;
                 var email = user.Email;
+
+
+                var users = new DbService().GetUsers();
+                ViewData["users"] = users;
+
+                BookModel model = new BookModel
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Lord of the Rings",
+                    AuthorName = "Джон Рональд Руэл Толкин",
+                    AuthorOriginalName = "John Ronald Reuel Tolkien",
+                    Year = 1955
+                };
+
+                // var books = _dbService.GetUserBooks(((BearlogPrincipal) User).Id);
+
+                return View(model);
             }
-
-            var users = new DbService().GetUsers();
-            ViewData["users"] = users;
-
-            return View();
+            else
+            {
+                return View();
+            }
         }
+   
     }
 }
