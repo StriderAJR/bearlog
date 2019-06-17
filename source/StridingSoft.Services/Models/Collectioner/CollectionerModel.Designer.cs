d35f664ab6db0f361e4cde53e73ca99a8d84a9eb
Alexander Kirsanov <alexander.a.kirsanov@gmail.com>
2019-06-17 12:50:08 +0500

    cleanup

--- source/StridingSoft.Services/Models/Collectioner/CollectionerModel.Designer.cs
+++ source/StridingSoft.Services/Models/Collectioner/CollectionerModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\dev\StridingSoft.Rest\source\StridingSoft.Services\Models\Collectioner\CollectionerModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\dev\striding-soft\rest\source\StridingSoft.Services\Models\Collectioner\CollectionerModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
